e0112380e5c44d49ef763241693ac0dcdfa4f4a1
cybc1g23 <cybc1g23@soton.ac.uk>
2024-03-14 04:49:15 +0000

    change input field

--- packages/nextjs/app/swap/_components/AmountIn.tsx
+++ packages/nextjs/app/swap/_components/AmountIn.tsx
@@ -17,12 +17,12 @@ export const AmountIn: React.FC<AmountInProps> = ({ swapResult, onToken1AmountCh
       <div className="w-full h-[75%] rounded-md bg-gradient-to-r from-pink-500 via-blue-500 to-gray-900 p-1 mr-20">
         <div className="flex flex-col h-full w-full items-center justify-center bg-gray-950 back ">
           <div className="flex h-40 w-[80%] mt-20 mr-20 ml-20 bg-[#c3c0c0b2] back rounded-2xl">
-            <div className="ml-4 mt-10 [font-family:'Krona_One-Regular',Helvetica] font-normal text-white text-[50px] tracking-[0] leading-[75px] whitespace-nowrap">
-            {swapResult ? swapResult.toString() :  <input className="bg-transparent w-full" type="number" onChange={handleInputChange} />}            
+            <div className="ml-4 mt-10 font-mono font-normal text-white text-[50px] tracking-[0] leading-[75px] whitespace-nowrap">
+            {swapResult ? swapResult.toString() :  <input className="bg-transparent w-full" type="number" placeholder="Enter value" onChange={handleInputChange} />}            
             </div>
           </div>
           <div className="flex flex-row h-[40%] w-[80%] m-20 bg-[#bdaeae5e] back rounded-2xl">
-            <div className="ml-4 [font-family:'Krona_One-Regular',Helvetica] font-normal text-white text-[30px] tracking-[0] leading-[75px] whitespace-nowrap">
+            <div className="ml-4 font-mono font-normal text-white text-[30px] tracking-[0] leading-[75px] whitespace-nowrap">
               CFX
             </div>
             <button className="justify-end">
